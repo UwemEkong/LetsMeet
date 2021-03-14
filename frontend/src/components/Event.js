ca84e35177d3e33aa9d609b13c4b954841c77105
Tyler Gibbs <gibbstyler7@gmail.com>
2021-03-14 06:40:25 -0500

    truncate description and background color

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,24 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 function convertUnix(unix_timestamp) {
     var date = new Date(unix_timestamp);
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString("en-US")}`;
 }
+
+function getColor() {
+    return "hsla(" + ~~(360 * Math.random()) + "," +
+        "70%," +
+        "80%,1)";
+}
+
+function truncate(str, n) {
+    return (str.length > n) ? str.substr(0, n - 1) + '...' : str;
+};
+
 const Event = ({ name, time, description, group_url, url, location, attendees, image_url }) => {
     time = convertUnix(time).toUpperCase();
     name = name.toUpperCase();
     return (
-        <div style={{ borderTop: "2px solid gray", marginBottom: "20px", cursor:"pointer" }} className="container">
-            <div style={{marginTop:"20px"}} className="row">
-                <div style={{marginTop:"20px",height:"200px",border:"4px solid black", borderRadius: "10px",backgroundImage:`url(${image_url})`, backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center"}} className="col-2">
+        <div style={{ borderTop: "2px solid gray", marginBottom: "20px", cursor: "pointer" }} className="container">
+            <div style={{ marginTop: "20px" }} className="row">
+                <div style={{ marginTop: "20px", height: "200px", width: "200px", border: "4px solid black", borderRadius: "10px", backgroundImage: `url(${image_url})`, backgroundColor: getColor(), backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center" }} className="col-2">
                 </div>
                 <div className="col-10">
-                    <p style={{fontSize:"24px", color:"#737373"}} >Start Time: {time}<br/></p>
-                    <h1 style={{fontWeight:"bold"}}>{name}<br/></h1>
-                    <p style={{fontSize:"30px", color:"#737373"}}> {description}<br/></p>
-                    <p style={{fontSize:"24px"}}>{attendees} attendees </p>
-                   
+                    <p style={{ fontSize: "24px", color: "#737373" }} >Start Time: {time}<br /></p>
+                    <h1 style={{ fontWeight: "bold" }}>{name}<br /></h1>
+                    <p style={{ fontSize: "30px", color: "#737373" }}> {truncate(description, 128)}<br /></p>
+                    <p style={{ fontSize: "24px" }}>{attendees} attendees </p>
+
                 </div>
 
             </div>
--- frontend/src/components/Group.js
+++ frontend/src/components/Group.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
+function getColor() {
+    return "hsla(" + ~~(360 * Math.random()) + "," +
+        "70%," +
+        "80%,1)";
+}
+
+function truncate(str, n) {
+    return (str.length > n) ? str.substr(0, n - 1) + '...' : str;
+};
+
 const Group = ({ name, url, image_url, members, description, events }) => {
 
     name = name.toUpperCase();
     return (
         <div style={{ borderTop: "2px solid gray", marginBottom: "20px" }} className="container">
             <div style={{marginTop:"20px"}} className="row">
-                <div style={{marginTop:"20px",height:"200px",border:"4px solid black", borderRadius: "10px",backgroundImage:`url(${image_url})`, backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center"}} className="col-2">
+                <div style={{marginTop:"20px",height:"200px",width:"200px",border:"4px solid black", borderRadius: "10px",backgroundImage:`url(${image_url})`, backgroundColor: getColor(), backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center"}} className="col-2">
                 </div>
                 <div className="col-10">
                     <h1 style={{fontWeight:"bold"}}>{name}<br/></h1>
-                    <p style={{fontSize:"30px", color:"#737373"}}> {description}<br/></p>
+                    <p style={{fontSize:"30px", color:"#737373"}}> {truncate(description, 128)}<br/></p>
                     <p style={{fontSize:"24px"}}>{members} members | Public</p>
                 </div>
             </div>
