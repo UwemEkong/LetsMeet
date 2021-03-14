975d5c6ebc95ab4d9fdab84edc5c39ca04a166cb
Uwem Ekong <uweme6@gmail.com>
2021-03-14 08:27:04 -0500

    Added styles to the modal on the events page

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { Modal } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
 
 const EventsPage = ({ results, setResults, formParams }) => {
 
@@ -29,20 +30,30 @@ const EventsPage = ({ results, setResults, formParams }) => {
         })
     }
 
+    function getColor() {
+        return "hsla(" + ~~(360 * Math.random()) + "," +
+            "70%," +
+            "80%,1)";
+    }
+    
+
     return (
         <>
             <Modal show={Object.keys(highlighted).length !== 0} onHide={handleClose}>
-                <Modal.Header closeButton>
+                <Modal.Header>
                     <Modal.Title>{highlighted.name}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Header>
+                    <Image  style={{backgroundColor:getColor(), backgroundImage: `url(${highlighted.image_url})`,width:"200px",height:"200px", margin:"auto" }} src={highlighted.image_url} thumbnail />
+                </Modal.Header>
+                <Modal.Body>{highlighted.description}</Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
-          </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-          </Button>
+                    </Button>
+                    <Button href={highlighted.url} target="_blank" variant="info" onClick={handleClose}>
+                        Sign Up!
+                    </Button>
                 </Modal.Footer>
             </Modal>
 
@@ -53,7 +64,7 @@ const EventsPage = ({ results, setResults, formParams }) => {
                             <h3><a className="nav-link active">Events</a></h3>
                         </li>
                         <li class="nav-item">
-                            <h3 style={{cursor:"pointer"}}><a class="nav-link" onClick={getGroups}>Groups</a></h3>
+                            <h3 style={{ cursor: "pointer" }}><a class="nav-link" onClick={getGroups}>Groups</a></h3>
                         </li>
                     </ul>
                 </div>
